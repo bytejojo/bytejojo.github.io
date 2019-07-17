6173e025c39a69070be5a685a00fb0d19dd0aa43
happyzeroro <hao_xl@outlook.com>
2019-07-17 19:12:29 +0800

    'deploy'

--- src/main.js
+++ src/main.js
@@ -7,15 +7,22 @@ import Util from './lib/util'
 
 Vue.config.productionTip = false
 
+const _hmt = _hmt || [];
+setTimeout(() => {
+  (() => {
+    const hm = document.createElement('script');
+    hm.src = "https://hm.baidu.com/hm.js?308c2a1201455d8a0e28c5b83d29f0c3";
+    const s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+},50);
+
 router.beforeEach((to, from, next) => {
-  let _hmt = _hmt || [];
-  _hmt.push('_requirePlugin',  'UrlChangeTracker', {
-    shouldTrackUrlChange:  function (newPath, oldPath)  {
-      return newPath && oldPath;
-    }
-  });
   iview.LoadingBar.start();
   Util.title(to.meta.title);
+  if (to.path) {
+    _hmt.push(['_trackPageview', '/#' + to.fullPath]);
+  }
   next();
 })
 
